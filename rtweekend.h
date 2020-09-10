201c5d2fe10d5c41e5967a49ff8368a8882c967d
Roberto Paredes <roberto.paredes@klarna.com>
2020-09-10 10:46:29 +0200

    add antialiasing

--- rtweekend.h
+++ rtweekend.h
@@ -5,6 +5,8 @@
 #include <cstdlib>
 #include <limits>
 #include <memory>
+#include <cstdlib>
+#include <iostream>
 
 using std::make_shared;
 using std::shared_ptr;
@@ -15,7 +17,24 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees)
 {
-  return degrees * pi / 180.0;
+    return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+    return min + (max - min) * random_double();
+}
+
+inline double clamp(double x, double min, double max)
+{
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common headers
